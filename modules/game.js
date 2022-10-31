287b98ad46cb755fd714004e223a8377912ff4ae
Nikhil Kumar <developernik18@gmail.com>
2022-10-31 17:34:48 +0530

    added rank by the amount of scored points

--- modules/game.js
+++ modules/game.js
@@ -18,7 +18,7 @@ export class Game {
     this.text = new Text(this);
 
     this.speed = 3;
-  
+
     this.maxBullets = 40;
     this.bullets = [];
     this.enemies = [];
@@ -34,46 +34,61 @@ export class Game {
     this.gameOver = false;
 
     this.gameMusic = new Audio();
-    this.gameMusic.src = '../assets/sound/gameMusic/m1.mp3';
+    this.gameMusic.src = "../assets/sound/gameMusic/m1.mp3";
     this.playMusic = true;
 
     this.gameOverMusic = new Audio();
-    this.gameOverMusic.src = '../assets/sound/win.ogg';
+    this.gameOverMusic.src = "../assets/sound/win.ogg";
 
-    this.playOtherSounds = true;    
+    this.playOtherSounds = true;
 
     this.lfps = 0.1;
     this.newLevelTimer = 0;
     this.newLevelInterval = 1000 / this.lfps;
-
+    this.reachedRank = "Trainee";
+    this.ranks = [
+      "Flying Officer",
+      "Flight Lieutenant",
+      "Junior Officers",
+      "Squadron Leader",
+      "Wing Commander",
+      "Group Captain",
+      "Senior Officers",
+      "Air Commodore",
+      "Air Vice Marshal",
+      "Air Marshal",
+      "Air Chief Marshal",
+      "Marshal"
+    ];
   }
   update(deltaTime) {
     // if(this.input.keys.includes('d')) {
     //   this.debug = !this.debug;
     // }
-    
 
     this.background.update(deltaTime);
     this.player.update(deltaTime);
-    this.bullets.forEach(bullet => {
+    this.bullets.forEach((bullet) => {
       bullet.update(deltaTime);
-    })
-    this.bullets = this.bullets.filter(bullet => !bullet.markedForDeletion);
+    });
+    this.bullets = this.bullets.filter((bullet) => !bullet.markedForDeletion);
 
-    this.enemies.forEach(enemy => {
+    this.enemies.forEach((enemy) => {
       enemy.update(deltaTime);
-    })
+    });
 
-    this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
 
     this.collisionDetection.isPlayerDead();
     this.collisionDetection.isEnemyDead();
 
-    this.destructionFire.forEach(fire => {
+    this.destructionFire.forEach((fire) => {
       fire.update(deltaTime);
     });
 
-    this.destructionFire = this.destructionFire.filter(fire => !fire.markedForDeletion);
+    this.destructionFire = this.destructionFire.filter(
+      (fire) => !fire.markedForDeletion
+    );
 
     // For schedule enemy entry;
     this.#addNewEnemy(deltaTime);
@@ -84,24 +99,23 @@ export class Game {
 
     this.background.draw(context);
     this.player.draw(context);
-    this.bullets.forEach(bullet => {
+    this.bullets.forEach((bullet) => {
       bullet.draw(context);
-    })
+    });
 
-    this.enemies.forEach(enemy => {
+    this.enemies.forEach((enemy) => {
       enemy.draw(context);
-    })
-    
-    this.destructionFire.forEach(fire => {
+    });
+
+    this.destructionFire.forEach((fire) => {
       fire.draw(context);
     });
 
     this.text.draw(context);
     this.drawIfGameOver(context);
-
   }
   updateBullets() {
-    if(this.maxBullets > 0 ) {
+    if (this.maxBullets > 0) {
       this.bullets.push(new Bullets(this));
       this.maxBullets--;
     }
@@ -119,17 +133,42 @@ export class Game {
     this.maxBullets += 2;
   }
   drawIfGameOver(context) {
-    if(this.gameOver) {
-      if(this.score > this.scoreToWin) {
-        this.text.gameOverScreen(context, true);
+    if (this.gameOver) {
+      if (this.score < 100) {
         this.gameOverMusic.play();
+        return this.text.gameOverScreen(context, false);
+      } else if (this.score < 200) {
+        this.reachedRank = this.ranks[0];
+      } else if (this.score < 300) {
+        this.reachedRank = this.ranks[1];
+      } else if (this.score < 400) {
+        this.reachedRank = this.ranks[2];
+      } else if (this.score < 500) {
+        this.reachedRank = this.ranks[3];
+      } else if (this.score < 1000) {
+        this.reachedRank = this.ranks[4];
+      } else if (this.score < 1500) {
+        this.reachedRank = this.ranks[5];
+      } else if (this.score < 2000) {
+        this.reachedRank = this.ranks[6];
+      } else if (this.score < 2500) {
+        this.reachedRank = this.ranks[7];
+      } else if (this.score < 3000) {
+        this.reachedRank = this.ranks[8];
+      } else if (this.score < 4000) {
+        this.reachedRank = this.ranks[9];
+      } else if (this.score < 5000) {
+        this.reachedRank = this.ranks[10];
       } else {
-        this.text.gameOverScreen(context, false);
-      }
+        this.reachedRank = this.ranks[11];
+      } 
+
+      this.gameOverMusic.play();
+      return this.text.gameOverScreen(context, true);
     }
   }
   #addNewEnemy(deltaTime) {
-    if(this.enemyEntryTimer > this.enemyEntryInterval) {
+    if (this.enemyEntryTimer > this.enemyEntryInterval) {
       this.enemyEntryInterval = Math.random() * 2000;
 
       this.addNewEnemies();
@@ -139,7 +178,7 @@ export class Game {
     }
   }
   #goToNextLevel(deltaTime) {
-    if(this.newLevelTimer > this.newLevelInterval) {
+    if (this.newLevelTimer > this.newLevelInterval) {
       this.score += 100;
       this.maxBullets += 10;
       this.speed += 0.5;
@@ -149,7 +188,7 @@ export class Game {
     }
   }
   shouldMusicContinue() {
-    if(this.gameMusic.paused && !this.gameOver) {
+    if (this.gameMusic.paused && !this.gameOver) {
       this.gameMusic.play();
     }
   }
